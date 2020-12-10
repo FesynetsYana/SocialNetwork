41fd75f95cc12a6a1e60dfc4f29b81998de3fd8c
FesynetsYana <01032001fesynets@gmail.com>
2020-12-10 13:13:38 +0200

    Started to work with Redis

---
+++ DalRedis/RedisRepository/PostRepository.cs
@@ -0,0 +1,15 @@
+﻿using DAL;
+using DAL.Enteties;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalRedis.RedisRepository
+{
+    public class PostRepository
+    {
+
+    }
+}
