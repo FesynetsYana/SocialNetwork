16427f3c661f53668caf4ca5f3158f579895b696
FesynetsYana <01032001fesynets@gmail.com>
2020-12-16 01:02:43 +0200

    Add Redis examples

---
+++ DALRedis/PostRepository.cs
@@ -0,0 +1,67 @@
+﻿using DAL;
+using DAL.Enteties;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using ServiceStack.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALRedis
+{
+    class PostRepository
+    {
+
+        IMongoDatabase database;
+        IMongoCollection<Post> collection;
+        public PostRepository()
+        {
+            database = MongoConfigManager.GetDefaultDatabase();
+            collection = database.GetCollection<Post>(GetTableName());
+        }
+
+        private string GetTableName()
+        {
+            return "post";
+        }
+
+        public void Add(string host, Post post)
+        {
+            using (RedisClient redisClient = new RedisClient(host) )
+            {
+                redisClient.Set(post.Id.ToString(), post.Text);
+            }
+        }
+        public void Update(string host, ObjectId id, Post post)
+        {
+            using (RedisClient redisClient = new RedisClient(host))
+            {
+                redisClient.Set(id.ToString(), post.Text);
+            }
+            collection.ReplaceOne(p => p.Id == id, post);
+        }
+
+        public void UpdatePost(string host, ObjectId postId, string newTxt)
+        {
+            using (RedisClient redisClient = new RedisClient(host))
+            {
+                redisClient.Set(postId.ToString(), newTxt);
+            }
+        }
+
+        public void AddLike(string UserNickname, ObjectId postId)
+        {
+
+            var filter = Builders<Post>.Filter.Eq("_id", postId);
+            var update = Builders<Post>.Update.Inc("Like", 1);
+            collection.UpdateOne(filter, update);
+
+            update = Builders<Post>.Update.Push("PersonsWhoLike", UserNickname);
+            collection.UpdateOne(filter, update);
+
+        }
+
+    }
+}
---
+++ DALRedis/Program.cs
@@ -0,0 +1,23 @@
+﻿using DAL.Enteties;
+using MongoDB.Bson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DALRedis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PostRepository postRepository = new PostRepository();
+
+            string host = "localhost";
+            postRepository.Add(host, new Post());
+            postRepository.Update(host, new ObjectId(),new Post());
+            postRepository.UpdatePost(host, new ObjectId(),"someText");
+        }
+    }
+}
